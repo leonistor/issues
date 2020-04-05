493c2b5dd4fe362b32813b1b1c894e916382c026
Leo Nistor <leo.nistor@gmail.com>
2020-04-05 19:41:52 +0300

    documentation

--- mix.exs
+++ mix.exs
@@ -6,6 +6,8 @@ defmodule Issues.MixProject do
       app: :issues,
       escript: escript_config(),
       version: "0.1.0",
+      name: "Issues",
+      source_url: "https://github.com/leonistor/issues",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -24,7 +26,9 @@ defmodule Issues.MixProject do
     [
       {:httpoison, "~> 1.6"},
       {:poison, "~> 4.0"},
-      {:scribe, "~> 0.10.0"}
+      {:scribe, "~> 0.10.0"},
+      {:ex_doc, "~> 0.21.3"},
+      {:earmark, "~> 1.4"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
