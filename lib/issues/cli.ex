d0c5ca925799f5edc9bc3f507c97fc59ea7be192
Leo Nistor <leo.nistor@gmail.com>
2020-04-05 09:41:13 +0300

    start process

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -6,7 +6,9 @@ defmodule Issues.CLI do
   @default_count 4
 
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args()
+    |> process()
   end
 
   @doc """
@@ -36,5 +38,17 @@ defmodule Issues.CLI do
 
   def args_to_internal(_) do
     :help
+  end
+
+  def process(:help) do
+    IO.puts("""
+    usage: issues <user> <project> [count | #{@default_count}]
+    """)
+
+    System.halt(0)
+  end
+
+  def process({user, project, _count}) do
+    # Issues.GuthubIssues.fetch(user, project)
   end
 end
