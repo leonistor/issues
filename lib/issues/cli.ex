8ace0ad3c87ebcfb26a6bdba8409608851cc17f5
Leo Nistor <leo.nistor@gmail.com>
2020-04-05 11:26:23 +0300

    fetch issues and decode

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -49,6 +49,14 @@ defmodule Issues.CLI do
   end
 
   def process({user, project, _count}) do
-    # Issues.GuthubIssues.fetch(user, project)
+    Issues.GithubIssues.fetch(user, project)
+    |> decode_response()
+  end
+
+  def decode_response({:ok, body}), do: body
+
+  def decode_response({:error, error}) do
+    IO.puts("Error fetching from Github: #{error["message"]}")
+    System.halt(2)
   end
 end
